e166514ddccd57f1c5d2456121ab1b39a991614d
Mis1eader-dev <muhammad.luqman.faiq@gmail.com>
2024-02-29 22:33:22 +0300

    Add JSON to WebSocketRoom

--- inc/drogon-user/WebSocketRoom.hpp
+++ inc/drogon-user/WebSocketRoom.hpp
@@ -7,6 +7,9 @@
 // #include "Room.hpp"
 #include "drogon-user/User.hpp"
 // #include "User.hpp"
+#include <json/value.h>
+#include <string>
+#include <string_view>
 
 template<
 	class Self,
@@ -78,103 +81,193 @@ public:
 
 
 
-	static inline void notify(const drogon::WebSocketConnectionPtr& conn, const std::string& msg,
-		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
-	{
-		room_.notify(conn, msg, type);
-	}
 	static inline void notify(const drogon::WebSocketConnectionPtr& conn, const char* msg,
 		uint64_t len, const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
 	{
 		room_.notify(conn, msg, len, type);
 	}
-	static inline void notify(const Connect& connect, const std::string& msg,
+	static inline void notify(const drogon::WebSocketConnectionPtr& conn, std::string_view msg,
 		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
 	{
-		room_.notify(connect, msg, type);
+		room_.notify(conn, msg, type);
+	}
+	static inline void notify(const drogon::WebSocketConnectionPtr& conn, Json::Value& json,
+		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
+	{
+		room_.notify(conn, json, type);
+	}
+	static inline void notify(const drogon::WebSocketConnectionPtr& conn, const Json::Value* json,
+		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
+	{
+		room_.notify(conn, json, type);
 	}
 	static inline void notify(const Connect& connect, const char* msg,
 		uint64_t len, const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
 	{
 		room_.notify(connect, msg, len, type);
 	}
-	static inline void notify(const Message& message, const std::string& msg,
+	static inline void notify(const Connect& connect, std::string_view msg,
 		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
 	{
-		room_.notify(message, msg, type);
+		room_.notify(connect, msg, type);
+	}
+	static inline void notify(const Connect& connect, Json::Value& json,
+		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
+	{
+		room_.notify(connect, json, type);
+	}
+	static inline void notify(const Connect& connect, const Json::Value* json,
+		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
+	{
+		room_.notify(connect, json, type);
 	}
 	static inline void notify(const Message& message, const char* msg,
 		uint64_t len, const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
 	{
 		room_.notify(message, msg, len, type);
 	}
-
-	static inline void notify(const UserPtr& user, const std::string& msg,
+	static inline void notify(const Message& message, std::string_view msg,
 		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
 	{
-		room_.notify(user, msg, type);
+		room_.notify(message, msg, type);
 	}
+	static inline void notify(const Message& message, Json::Value& json,
+		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
+	{
+		room_.notify(message, json, type);
+	}
+	static inline void notify(const Message& message, const Json::Value* json,
+		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
+	{
+		room_.notify(message, json, type);
+	}
+
 	static inline void notify(const UserPtr& user, const char* msg,
 		uint64_t len, const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
 	{
 		room_.notify(user, msg, len, type);
 	}
-
-
-
-	static inline void notifyAll(const std::string& msg,
+	static inline void notify(const UserPtr& user, std::string_view msg,
 		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
 	{
-		room_.notifyAll(msg, type);
+		room_.notify(user, msg, type);
 	}
+	static inline void notify(const UserPtr& user, Json::Value& json,
+		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
+	{
+		room_.notify(user, json, type);
+	}
+	static inline void notify(const UserPtr& user, const Json::Value* json,
+		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
+	{
+		room_.notify(user, json, type);
+	}
+
+
+
 	static inline void notifyAll(const char* msg, uint64_t len,
 		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
 	{
 		room_.notifyAll(msg, len, type);
 	}
-
-
-
-	static inline void notifyAllExcept(const UserPtr& user, const std::string& msg,
+	static inline void notifyAll(std::string_view msg,
 		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
 	{
-		room_.notifyAllExcept(user, msg, type);
+		room_.notifyAll(msg, type);
 	}
+	static inline void notifyAll(Json::Value& json,
+		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
+	{
+		room_.notifyAll(json, type);
+	}
+	static inline void notifyAll(const Json::Value* json,
+		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
+	{
+		room_.notifyAll(json, type);
+	}
+
+
+
 	static inline void notifyAllExcept(const UserPtr& user, const char* msg,
 		uint64_t len, const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
 	{
 		room_.notifyAllExcept(user, msg, len, type);
 	}
-
-	static inline void notifyAllExcept(const drogon::WebSocketConnectionPtr& conn, const std::string& msg,
+	static inline void notifyAllExcept(const UserPtr& user, std::string_view msg,
 		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
 	{
-		room_.notifyAllExcept(conn, msg, type);
+		room_.notifyAllExcept(user, msg, type);
 	}
+	static inline void notifyAllExcept(const UserPtr& user, Json::Value& json,
+		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
+	{
+		room_.notifyAllExcept(user, json, type);
+	}
+	static inline void notifyAllExcept(const UserPtr& user, const Json::Value* json,
+		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
+	{
+		room_.notifyAllExcept(user, json, type);
+	}
+
 	static inline void notifyAllExcept(const drogon::WebSocketConnectionPtr& conn, const char* msg,
 		uint64_t len, const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
 	{
 		room_.notifyAllExcept(conn, msg, len, type);
 	}
-	static inline void notifyAllExcept(const Connect& connect, const std::string& msg,
+	static inline void notifyAllExcept(const drogon::WebSocketConnectionPtr& conn, std::string_view msg,
 		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
 	{
-		room_.notifyAllExcept(connect, msg, type);
+		room_.notifyAllExcept(conn, msg, type);
+	}
+	static inline void notifyAllExcept(const drogon::WebSocketConnectionPtr& conn, Json::Value& json,
+		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
+	{
+		room_.notifyAllExcept(conn, json, type);
+	}
+	static inline void notifyAllExcept(const drogon::WebSocketConnectionPtr& conn, const Json::Value* json,
+		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
+	{
+		room_.notifyAllExcept(conn, json, type);
 	}
 	static inline void notifyAllExcept(const Connect& connect, const char* msg,
 		uint64_t len, const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
 	{
 		room_.notifyAllExcept(connect, msg, len, type);
 	}
-	static inline void notifyAllExcept(const Message& message, const std::string& msg,
+	static inline void notifyAllExcept(const Connect& connect, std::string_view msg,
 		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
 	{
-		room_.notifyAllExcept(message, msg, type);
+		room_.notifyAllExcept(connect, msg, type);
+	}
+	static inline void notifyAllExcept(const Connect& connect, Json::Value& json,
+		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
+	{
+		room_.notifyAllExcept(connect, json, type);
+	}
+	static inline void notifyAllExcept(const Connect& connect, const Json::Value* json,
+		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
+	{
+		room_.notifyAllExcept(connect, json, type);
 	}
 	static inline void notifyAllExcept(const Message& message, const char* msg,
 		uint64_t len, const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
 	{
 		room_.notifyAllExcept(message, msg, len, type);
+	}
+	static inline void notifyAllExcept(const Message& message, std::string_view msg,
+		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
+	{
+		room_.notifyAllExcept(message, msg, type);
+	}
+	static inline void notifyAllExcept(const Message& message, Json::Value& json,
+		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
+	{
+		room_.notifyAllExcept(message, json, type);
+	}
+	static inline void notifyAllExcept(const Message& message, const Json::Value* json,
+		const drogon::WebSocketMessageType type = drogon::WebSocketMessageType::Text)
+	{
+		room_.notifyAllExcept(message, json, type);
 	}
 
 
